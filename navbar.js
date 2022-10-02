605cdd0384b282c9f98ee5420bbc0350eeacebad
Sarunnanimasai <103571317+Sarunnanimasai@users.noreply.github.com>
2022-10-02 09:31:39 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,5 +1,41 @@
-import {nav} from './component/nav.js'
-console.log(nav())
- let div_na=document.getElementById("n")
+let bakery = document.getElementById("bakery");
+let fruits = document.getElementById("fruits");
+let grains = document.getElementById("grains");
+let snacks = document.getElementById("snacks");
+let beauty = document.getElementById("beauty");
+let beverages = document.getElementById("beverages");
+let cart = document.getElementById("basket_cart");
+let logo = document.getElementById("big_b_logo");
 
- div_na.innerHTML=nav();
+
+bakery.addEventListener("click",(e)=>{
+    window.location.href = "./bakery.html"
+})
+
+fruits.addEventListener("click",(e)=>{
+    window.location.href = "./fruits.html"
+})
+
+grains.addEventListener("click",(e)=>{
+    window.location.href = "./grains.html"
+})
+
+snacks.addEventListener("click",(e)=>{
+    window.location.href = "./snacks.html"
+})
+
+beauty.addEventListener("click",(e)=>{
+    window.location.href = "./beauty.html"
+})
+
+beverages.addEventListener("click",(e)=>{
+    window.location.href = "./beverages.html"
+})
+
+cart.addEventListener("click",(e)=>{
+    window.location.href = "./cart.html"
+})
+
+logo.addEventListener("click",(e)=>{
+    window.location.href = "./homepage.html"
+})
